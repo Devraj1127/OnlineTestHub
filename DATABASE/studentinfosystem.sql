0976d2f58335172ccfeebda9a2ad073cc19210f4
Devraj <132833114+Devraj1127@users.noreply.github.com>
2023-05-11 17:59:45 +0530

    Update studentinfosystem.sql

--- DATABASE/studentinfosystem.sql
+++ DATABASE/studentinfosystem.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Dec 24, 2018 at 03:55 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
